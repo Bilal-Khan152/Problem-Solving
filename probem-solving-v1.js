319156700d7c089ed011241886aa4cd271869da9
Muhammad Bilal <bilalkhan16496@gmail.com>
2026-07-25 23:55:51 +0500

    moved all zero in to the end

--- probem-solving-v1.js
+++ probem-solving-v1.js
@@ -1,38 +1,55 @@
 // extract max number from any array of numbers
 
-const findMaxNum = (arr) => {
-  let maxNum = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > maxNum) {
-      maxNum = arr[i];
-    }
-  }
+// const findMaxNum = (arr) => {
+//   let maxNum = arr[0];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > maxNum) {
+//       maxNum = arr[i];
+//     }
+//   }
 
-  return maxNum;
-};
+//   return maxNum;
+// };
 
-const arr = [-2, -1, -19];
-console.log(findMaxNum(arr));
+// const arr = [-2, -1, -19];
+// console.log(findMaxNum(arr));
 
 // find the missing number from the given array (sorted array) and return that number only
 
-const findMissigNum = (arr) => {
-  let properArr = [];
-  let missingNumber;
+// const findMissigNum = (arr) => {
+//   let properArr = [];
+//   let missingNumber;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     properArr.push(i + 1);
+//   }
+
+//   for (let i = 0; i < properArr.length; i++) {
+//     if (properArr[i] !== arr[i]) {
+//       missingNumber = properArr[i];
+//       break;
+//     }
+//   }
+
+//   return missingNumber;
+// };
+
+// const arr = [1, 2, 3, 4, 5, 7, 8, 9, 10];
+// console.log(findMissigNum(arr));
+
+ 
+ const moveZerosToEnd = (arr) => {
+  let j = 0;
 
   for (let i = 0; i < arr.length; i++) {
-    properArr.push(i + 1);
-  }
-
-  for (let i = 0; i < properArr.length; i++) {
-    if (properArr[i] !== arr[i]) {
-      missingNumber = properArr[i];
-      break;
+    if (arr[i] !== 0) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      j++;
     }
   }
 
-  return missingNumber;
+  return arr;
 };
 
-const arr = [1, 2, 3, 4, 5, 7, 8, 9, 10];
-console.log(findMissigNum(arr));
+const arr = [0, 1, 0, 3, 12];
+console.log(moveZerosToEnd(arr));
